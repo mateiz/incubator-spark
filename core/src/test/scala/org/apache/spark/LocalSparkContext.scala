c24f3bff2f550de15591837f76fa296c143e090a
Evan Chan <ev@ooyala.com>
2013-10-11 16:11:44 -0700

    No need to clear the system property anymore!

--- core/src/test/scala/org/apache/spark/LocalSparkContext.scala
+++ core/src/test/scala/org/apache/spark/LocalSparkContext.scala
@@ -52,7 +52,6 @@ object LocalSparkContext {
       sc.stop()
     }
     // To avoid Akka rebinding to the same port, since it doesn't unbind immediately on shutdown
-    System.clearProperty("spark.driver.port")
     System.clearProperty("spark.hostPort")
   }
 
